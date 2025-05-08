fd4b647175d44d17dd88ac3bbc9173d78e89311a
gabbyp30 <gabrielle.pittman@mscoding.org>
2025-05-08 10:42:50 -0500

    Day 11 Javascript
    Defining and Invoking Functions

---
+++ JS/app.js
@@ -0,0 +1,26 @@
+//Problem 1
+
+
+function greetUser(name) {
+    return "Hello," + name + "!";
+}
+console.log(greetUser("Gabby"));
+
+//Problem 2
+
+function calculateArea(length, width) {
+    return length * width;
+}
+console.log(calculateArea(10, 20));
+
+//Problem 3
+
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+
+console.log(addNumbers(30, 28))
+
+//Problem 4
+
+fuction 
